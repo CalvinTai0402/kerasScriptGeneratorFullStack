3389031da03320fef0ba1649321a3efe41910fcf
CalvinTai0402 <jfctai@rams.colostate.edu>
2021-07-06 17:26:08 -0600

    update URL

--- resources/js/components/DownloadUploadModels/DownloadUploadModelsIndex.js
+++ resources/js/components/DownloadUploadModels/DownloadUploadModelsIndex.js
@@ -81,6 +81,8 @@ class DownloadUploadModelsIndex extends React.Component {
         const { deleting } = this.state;
         let self = this;
         let url = ""
+        console.log(process.env.MIX_API_URL)
+        console.log(process.env.APP_ENV)
         if (process.env.MIX_APP_ENV === "production") {
             url = `${process.env.MIX_API_URL}/kerasModel`;
         } else {
